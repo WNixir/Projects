1223cc948fc597f91e0a48c1676d5785a049be4f
Rixin Wang <30443826+WNixir@users.noreply.github.com>
2018-03-24 15:53:27 -0400

    Update TOAHModel.py

--- ToH/TOAHModel.py
+++ ToH/TOAHModel.py
@@ -2,8 +2,6 @@
 # Dustin Wehr, Brian Harrington, Rixin Wang
 # Distributed under the terms of the GNU General Public License.
 #
-# This file is part of Assignment 2, CSCA48, Summer 2017.
-#
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
